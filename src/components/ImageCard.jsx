92a82f3272d101ae3ba20410d68ae6cf7a09f4f7
jazsouf <101473962+jazsouf@users.noreply.github.com>
2023-06-26 12:59:40 +0200

    integrate slider

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,7 +1,88 @@
 import React from "react";
+import { useKeenSlider } from "keen-slider/react";
+import "./testSliderStyle.css";
+import "./ImageCard.css";
 
-const ImageCard = () => {
-  return <div>ImageCard</div>;
+const ImageCard = ({ direction }) => {
+  const WheelControls = (slider) => {
+    let touchTimeout;
+    let position;
+    let wheelActive;
+
+    function dispatch(e, name) {
+      position.x -= e.deltaX;
+      if (direction === "top") {
+        position.y -= e.deltaY;
+      }
+      if (direction === "down") {
+        position.y += e.deltaY;
+      }
+      slider.container.dispatchEvent(
+        new CustomEvent(name, {
+          detail: {
+            x: position.x,
+            y: position.y,
+          },
+        })
+      );
+    }
+
+    function wheelStart(e) {
+      position = {
+        x: e.pageX,
+        y: e.pageY,
+      };
+
+      dispatch(e, "ksDragStart");
+    }
+
+    function wheel(e) {
+      dispatch(e, "ksDrag");
+    }
+
+    function wheelEnd(e) {
+      dispatch(e, "ksDragEnd");
+    }
+
+    function eventWheel(e) {
+      e.preventDefault();
+      if (!wheelActive) {
+        wheelStart(e);
+        wheelActive = true;
+      }
+      wheel(e);
+      clearTimeout(touchTimeout);
+      touchTimeout = setTimeout(() => {
+        wheelActive = false;
+        wheelEnd(e);
+      }, 50);
+    }
+
+    slider.on("created", () => {
+      window.addEventListener("wheel", eventWheel, {
+        passive: false,
+      });
+    });
+  };
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+      rubberband: false,
+      vertical: true,
+    },
+    [WheelControls]
+  );
+
+  return (
+    <div ref={sliderRef} className="keen-slider">
+      <div className="keen-slider__slide number-slide1">1</div>
+      <div className="keen-slider__slide number-slide2">2</div>
+      <div className="keen-slider__slide number-slide3">3</div>
+      <div className="keen-slider__slide number-slide4">4</div>
+      <div className="keen-slider__slide number-slide5">5</div>
+      <div className="keen-slider__slide number-slide6">6</div>
+    </div>
+  );
 };
 
 export default ImageCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import TopBar from "./../components/TopBar";
 import ImageCard from "./../components/ImageCard";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div>
       <TopBar />
-      <ImageCard />
-      <ImageCard />
+      <div className="images-slider">
+        <ImageCard direction="top" />
+        <ImageCard direction="down" />
+      </div>
     </div>
   );
 };
