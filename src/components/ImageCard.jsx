2d76d424ae0c5b40a12445a87275522b3ce8cf80
jazsouf <101473962+jazsouf@users.noreply.github.com>
2023-06-26 15:04:12 +0200

    add images

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -75,12 +75,20 @@ const ImageCard = ({ direction }) => {
 
   return (
     <div ref={sliderRef} className="keen-slider">
-      <div className="keen-slider__slide number-slide1">1</div>
-      <div className="keen-slider__slide number-slide2">2</div>
-      <div className="keen-slider__slide number-slide3">3</div>
-      <div className="keen-slider__slide number-slide4">4</div>
-      <div className="keen-slider__slide number-slide5">5</div>
-      <div className="keen-slider__slide number-slide6">6</div>
+      {direction === "left"
+        ? Array.from({ length: 10 }, (_, i) => (
+            <div className="keen-slider__slide number-slide1" key={i}>
+              <img src={`./../../public/images/IMAGE_${2 * i}.png`} alt="" />
+            </div>
+          ))
+        : Array.from({ length: 10 }, (_, i) => (
+            <div className="keen-slider__slide number-slide1" key={i}>
+              <img
+                src={`./../../public/images/IMAGE_${2 * i + 1}.png`}
+                alt=""
+              />
+            </div>
+          ))}
     </div>
   );
 };
