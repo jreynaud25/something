b4f0e9b095568d8ebd44caa7e985b9a95c930201
jazsouf <101473962+jazsouf@users.noreply.github.com>
2023-06-26 11:54:55 +0200

    Setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
